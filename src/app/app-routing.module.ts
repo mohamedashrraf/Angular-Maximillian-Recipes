44f7e598b995b3277b2135c5ac8ed7bae0a06dff
mohamed-ashrraf <mohamed-ashraf@spatiumsoftware.com>
2024-04-18 17:56:42 +0200

    modules & Deploying

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+
   { path:'recipes', component: RecipesComponent, canActivate: [AuthGuard],
      children: [
       { path: '', component: RecipeStartComponent},
@@ -19,6 +20,23 @@ const routes: Routes = [
     ]},
   { path:'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent }
+
+  // {
+  //   path: "recipes",
+  //   loadChildren: () =>
+  //     import("./recipes/recipes.module").then(m => m.RecipesModule)
+  // },
+  // {
+  //   path: "shopping-list",
+  //   loadChildren: () =>
+  //     import("./shopping-list/shopping-list.module").then(
+  //       m => m.ShoppingListModule
+  //     )
+  // },
+  // {
+  //   path: "auth",
+  //   loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
+  // }
 ];
 
 @NgModule({
